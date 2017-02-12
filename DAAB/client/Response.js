4034f6c999b87c8cb28592668981443842aba277
Dash-Wedergren <dashwedergren@gmail.com>
2017-02-12 11:26:26 -0600

    Live data on survey Response

--- DAAB/client/Response.js
+++ DAAB/client/Response.js
@@ -10,32 +10,47 @@ Template.surveys.helpers({
 		var currentSurvey = survey.findOne({surveyId: FlowRouter.getParam("surveyId")});
 		console.log(currentSurvey)
 		var list = []
+		var i = 0;
 		currentSurvey.question.forEach(function(value, index){
 			if(value.type == "freeResponse"){
+				value.id = i;
 				list.push(value)
+				i++;
 			}
 		});
 		return list
 	},
 	yesN: function(){
 		var currentSurvey = survey.findOne({surveyId: FlowRouter.getParam("surveyId")});
-		var list = []
+		var list = [];
+		var i = 0;
 		currentSurvey.question.forEach(function(value, index){
 			if(value.type == "yesNo"){
-				list.push(value)
+				value.id = i;
+				list.push(value);
+				i++;
 			}
 		});
 		return list
 	},
 	multipleC: function(){
 		var currentSurvey = survey.findOne({surveyId: FlowRouter.getParam("surveyId")});
-		console.log(currentSurvey)
-		var list = []
+		var list = [];
+		var i = 0;
 		currentSurvey.question.forEach(function(value, index){
 			if(value.type == "multipleChoice"){
-				list.push(value)
+				value.id = i;
+				list.push(value);
+				i++;
 			}
 		});
+		console.log(list)
 		return list
 	}
 });
+
+Template.surveys.events({
+	'click #submit'(event){
+
+	}
+})
